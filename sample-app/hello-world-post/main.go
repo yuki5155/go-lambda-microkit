3e3e113998f8a0067b8719bff81f3b62cf91efd5
Yuki5155 <yukiasano55@gmail.com>
2024-09-17 12:08:08 +0000

    postのライブラリまで

--- sample-app/hello-world-post/main.go
+++ sample-app/hello-world-post/main.go
@@ -1,55 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+
+	"hello-world-post/models"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/yuki5155/go-lambda-microkit/utils"
 )
 
-type Body struct {
-	TestBody string `json:"testBody"` // JSONのキーに合わせて小文字に変更
-}
-
-type ResponseBody struct {
-	Response string `json:"response"`
-}
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	body, err := utils.NewRequestBody[Body](request, Body{})
+	body, err := utils.NewRequestBody[models.RequestBody](request, models.RequestBody{})
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 400,
-			Body:       "Bad Request",
-		}, nil
+		return utils.BadRequestResponse()
 	}
 
 	if body.TestBody == "" {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 400,
-			Body:       "Bad Request",
-		}, nil
+		return utils.BadRequestResponse("TestBody cannot be empty")
 	}
 
-	resBody := ResponseBody{
+	resBody := models.ResponseBody{
 		Response: fmt.Sprintf("Received TestBody: %s", body.TestBody),
 	}
 
-	resBodyJson, err := json.Marshal(resBody)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Internal Server Error",
-		}, nil
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       string(resBodyJson),
-		StatusCode: 200,
-	}, nil
+	return utils.SuccessResponse(resBody)
 }
 
 func main() {
