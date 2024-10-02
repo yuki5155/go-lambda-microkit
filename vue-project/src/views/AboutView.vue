b93c657f030c3bfac5ef43d0043d382a9712830f
Yuki5155 <yukiasano55@gmail.com>
2024-10-02 20:30:49 +0000

    プロジェクト立ち上げ

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello, Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
